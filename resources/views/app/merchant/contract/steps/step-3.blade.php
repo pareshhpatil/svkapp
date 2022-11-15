bef61fef08c1809e7bde1dd80b0fab0a6fea6a6b
Meet Thosar <meet.thosar@briq.com>
2022-11-15 12:28:52 +0530

    Grand Total added in the preview screen

--- resources/views/app/merchant/contract/steps/step-3.blade.php
+++ resources/views/app/merchant/contract/steps/step-3.blade.php
@@ -124,6 +124,18 @@
                         </tr>
                     @endforeach
                     </tbody>
+                    <tfoot>
+                        <tr class="warning">
+                            <th class="td-c"></th>
+                            <th class="td-c">Grand total</th>
+                            <th class="td-c">
+                                <span id="particulartotaldiv">{{ number_format($contract->contract_amount) }}</span>
+                                <input type="hidden" id="particulartotal" data-cy="particular-total1" name="contract_amount" value="{{ $contract->contract_amount }}">
+                            </th>
+                            <th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th>
+
+                        </tr>
+                    </tfoot>
                 </table>
             </div>
 
