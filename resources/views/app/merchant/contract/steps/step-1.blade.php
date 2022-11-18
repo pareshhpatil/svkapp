84eeff27190ff8241efa8e9dde20b2ef6321f952
Meet Thosar <meet.thosar@briq.com>
2022-11-18 18:55:00 +0530

    Working on contract particular scroll

--- resources/views/app/merchant/contract/steps/step-1.blade.php
+++ resources/views/app/merchant/contract/steps/step-1.blade.php
@@ -131,12 +131,12 @@
 <div class="portlet light bordered">
     <div class="portlet-body form">
     <h3 class="form-section">
-        Application and Certificate for Payment
+        Address information
     </h3>
     <div class="row">
         <div class="col-md-6">
             <div class="form-group">
-                <label class="control-label col-md-4">To Owner<span class="required">*</span></label>
+                <label class="control-label col-md-4">Owner<span class="required">*</span></label>
                 <div class="col-md-8">
                     <textarea class="form-control" name="owner_address" id="owner_address" rows="3" required >{{ $contract->owner_address??old('owner_address') }}</textarea>
                     @error('owner_address')<div class="text-danger">{{ $message }}</div>@enderror
@@ -158,7 +158,7 @@
     <div class="row">
         <div class="col-md-6">
             <div class="form-group">
-                <label class="control-label col-md-4">From Contractor<span class="required">*</span></label>
+                <label class="control-label col-md-4">Contractor<span class="required">*</span></label>
                 <div class="col-md-8">
                     <textarea class="form-control" name="contractor_address" id="contractor_address" rows="3" required>{{ $contract->contractor_address??old('contractor_address') }}</textarea>
                     @error('contractor_address')<div class="text-danger">{{ $message }}</div>@enderror
@@ -168,7 +168,7 @@
         </div>
         <div class="col-md-6">
             <div class="form-group">
-                <label class="control-label col-md-4">Via Architect<span class="required">*</span></label>
+                <label class="control-label col-md-4">Architect<span class="required">*</span></label>
                 <div class="col-md-8">
                     <textarea class="form-control" name="architect_address" id="architect_address" rows="3" required>{{ $contract->architect_address??old('architect_address') }}</textarea>
                     @error('architect_address')<div class="text-danger">{{ $message }}</div>@enderror
--- resources/views/app/merchant/contract/steps/step-2-footer.blade.php
+++ resources/views/app/merchant/contract/steps/step-2-footer.blade.php
@@ -1,7 +1,6 @@
 <tfoot>
 <tr class="warning">
-    <th class="td-c"></th>
-    <th class="td-c">Grand total</th>
+    <th class="td-c" colspan="2">Grand total</th>
     <th class="td-c">
         <span id="particulartotaldiv">{{ number_format($contract->contract_amount) }}</span>
         <input type="hidden" id="particulartotal" data-cy="particular-total1" name="contract_amount" value="{{ $contract->contract_amount }}">
