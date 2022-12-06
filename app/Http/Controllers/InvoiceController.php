c7a8ae333a1edeaf74fe938b2eafa54b1cadb998
Paresh <paresh.patil@briq.com>
2022-12-06 13:06:45 +0530

    link change

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3080,6 +3080,7 @@ SQL;
                 foreach ($cop_particulars as $kdata) {
                     if ($kdata["bill_code"] == $key) {
                         $kdata["cost_type"]=isset($kdata["cost_type"])? $kdata["cost_type"] : '';
+                        
                         $co_particulars[] = array('bill_code' => $key, 'change_order_amount' => array_sum($value), 'description' =>  $kdata["description"], 'cost_type' =>  $kdata["cost_type"]);
                     }
                 }
