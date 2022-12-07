7f68286506a1cd5df9feb30332647daeed8ae11b
nitishswipezbriq <nitish.harchand@briq.com>
2022-12-07 11:03:20 +0530

    debug download pdf logo

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -1691,6 +1691,7 @@ class InvoiceController extends AppController
                 define("DOMPDF_DPI", 120);
                 define("DOMPDF_ENABLE_REMOTE", true);
                 if ($info['template_type'] == 'construction') {
+                    dd($data);
                     $pdf = DOMPDF::loadView('mailer.invoice.format-' . $type, $data);
                     $pdf->setPaper("a4", "landscape");
                 } else {
