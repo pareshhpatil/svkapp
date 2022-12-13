e9429f89dd84213814a1484f82a7d4c42c379947
nitishswipezbriq <nitish.harchand@briq.com>
2022-12-13 23:17:34 +0530

    debug img path

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -842,7 +842,7 @@ class InvoiceController extends AppController
             //end code for new design
             $banklist = $this->parentModel->getConfigList('Bank_name');
             $banklist = json_decode($banklist, 1);
-            
+            dd($info['image_path']);
             $imgpath = env('APP_URL') . '/uploads/images/logos/' . $info['image_path'] ?? '';
 
             if (isset($info['image_path'])) {
