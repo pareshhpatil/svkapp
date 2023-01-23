ed81f91352dc9bf76dabc89ded91b52beef093a2
darshanaB <darshana@swipez.in>
2023-01-23 13:34:05 +0530

    fixed issue in contract update

--- resources/views/app/merchant/contract/steps/stable-step-2.blade.php
+++ resources/views/app/merchant/contract/steps/stable-step-2.blade.php
@@ -327,7 +327,7 @@
 
         function handle_particulars(){
             return {
-                fields : JSON.parse('{!! json_encode($particulars) !!}'),
+                fields : JSON.parse('{!! particularJson !!}'),
                 bill_code : null,
                 bill_description : null,
                 group_name : null,
