7e690d08baeda6bb8b99ea85b2a69f4bf3f06551
nitishswipezbriq <117810150+nitishswipezbriq@users.noreply.github.com>
2023-02-22 15:43:18 +0530

    Update invoice_footer.blade.php

--- resources/views/app/merchant/invoiceformat/invoice_footer.blade.php
+++ resources/views/app/merchant/invoiceformat/invoice_footer.blade.php
@@ -985,6 +985,7 @@ $(function(){
         // let notifyPatronStatus = invoicePreviewForm.find('input[name=notify_patron]');
         paymentRequestStatus.val(0);
         // notifyPatronStatus.val(1);
+        //test
         invoicePreviewForm.submit();
     })
 })
