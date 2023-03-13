7a506c22246b665df417f81b45e97454d3f6696a
nitishswipezbriq <nitish.harchand@briq.com>
2023-03-13 21:55:44 +0530

    test queue job

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\TestJob;
+use Illuminate\Console\Command;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:job';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        TestJob::dispatch()->onQueue('promotion-sms-dev');
+    }
+}
---
+++ app/Jobs/TestJob.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Constants\Models\IColumn;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+
+class TestJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        DB::table('briq_roles')
+            ->insert([
+                'merchant_id' => 'M000000041',
+                'name' => "Job Role",
+                'description' => "Test Job Role",
+                'created_by' => 'U000000165',
+                'last_updated_by' => 'U000000165',
+                IColumn::CREATED_AT  => Carbon::now()->toDateTimeString(),
+                IColumn::UPDATED_AT  => Carbon::now()->toDateTimeString()
+            ]);
+    }
+}
