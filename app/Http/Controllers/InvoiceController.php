bc215c6e58945082a1037d0c08753f3fcadaba2a
nitishswipezbriq <nitish.harchand@briq.com>
2023-03-15 14:33:22 +0530

    debug

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3094,17 +3094,47 @@ class InvoiceController extends AppController
         $data['has_aia_license'] = $hasAIALicense;
 
         $invoicePrivilegesAccessIDs = json_decode(Redis::get('invoice_privileges_' . $this->user_id), true);
-        $projectPrivilegesAccessIDs = json_decode(Redis::get('project_privileges_' . $this->user_id), true);
+        //$projectPrivilegesAccessIDs = json_decode(Redis::get('project_privileges_' . $this->user_id), true);
         $contractPrivilegesAccessIDs = json_decode(Redis::get('contract_privileges_' . $this->user_id), true);
         $invoiceAccess = '';
-        dd($contractPrivilegesAccessIDs, $info);
+
 //        if(!empty($projectPrivilegesAccessIDs) && in_array($info['project_id'], array_keys($projectPrivilegesAccessIDs))) {
 //            if($projectPrivilegesAccessIDs[$info['project_id']] == 'full') {
 //                $invoiceAccess = 'full';
 //            }
 //        }
 
-        if(in_array('all', array_keys($invoicePrivilegesAccessIDs))) {
+        if (in_array($info['payment_request_id'], array_keys($invoicePrivilegesAccessIDs))) {
+            if($invoicePrivilegesAccessIDs[$info['payment_request_id']] == 'full') {
+                $invoiceAccess = 'full';
+            }
+            if($invoicePrivilegesAccessIDs[$info['payment_request_id']] == 'edit') {
+                $invoiceAccess = 'edit';
+            }
+            if($invoicePrivilegesAccessIDs[$info['payment_request_id']] == 'view-only') {
+                $invoiceAccess = 'view-only';
+            }
+            if($invoicePrivilegesAccessIDs[$info['payment_request_id']] == 'approve') {
+                $invoiceAccess = 'approve';
+            }
+        } elseif(in_array($info['contract_id'], array_keys($contractPrivilegesAccessIDs))) {
+            if($contractPrivilegesAccessIDs[$info['contract_id']] == 'full') {
+                $invoiceAccess = 'full';
+            }
+
+            if($contractPrivilegesAccessIDs[$info['contract_id']] == 'edit') {
+                $invoiceAccess = 'edit';
+            }
+
+            if($contractPrivilegesAccessIDs[$info['contract_id']] == 'approve') {
+                $invoiceAccess = 'approve';
+            }
+
+            if($contractPrivilegesAccessIDs[$info['contract_id']] == 'view-only') {
+                $invoiceAccess = 'view-only';
+            }
+
+        } elseif(in_array('all', array_keys($invoicePrivilegesAccessIDs))) {
             if($invoicePrivilegesAccessIDs['all'] == 'full') {
                 $invoiceAccess = 'full';
             }
@@ -3112,21 +3142,16 @@ class InvoiceController extends AppController
             if($invoicePrivilegesAccessIDs['all'] == 'edit') {
                 $invoiceAccess = 'edit';
             }
-        } elseif (in_array($info['payment_request_id'], array_keys($invoicePrivilegesAccessIDs))) {
-            if($invoicePrivilegesAccessIDs[$info['payment_request_id']] == 'full') {
-                $invoiceAccess = 'full';
-            } elseif($invoicePrivilegesAccessIDs[$info['payment_request_id']] == 'edit') {
-                $invoiceAccess = 'edit';
-            } elseif($invoicePrivilegesAccessIDs[$info['payment_request_id']] == 'view-only') {
+
+            if($invoicePrivilegesAccessIDs['all'] == 'view-only') {
                 $invoiceAccess = 'view-only';
-            } elseif($invoicePrivilegesAccessIDs[$info['payment_request_id']] == 'approve') {
+            }
+
+            if($invoicePrivilegesAccessIDs['all'] == 'approve') {
                 $invoiceAccess = 'approve';
             }
-        } 
-//        elseif(in_array('edit', array_values($invoicePrivilegesAccessIDs)) || in_array('approve', array_values($invoicePrivilegesAccessIDs))) {
-//            $invoiceAccess = 'edit';
-//        }
-        dd($invoiceAccess);
+        }
+
         $data['invoice_access'] = $invoiceAccess;
 
         return $data;
