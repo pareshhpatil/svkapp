afeba67012da4c8e4b0d3c038a41bd48dad0a463
nitishswipezbriq <nitish.harchand@briq.com>
2023-03-21 18:56:21 +0530

    debug dwnlod full patron

--- routes/web.php
+++ routes/web.php
@@ -517,10 +517,10 @@ Route::group(['prefix' => 'patron'], function () {
   Route::get('invoice/view/{link}', 'InvoiceController@patronView');
   Route::get('invoice/document/download/{link}', 'InvoiceController@downloadSingle');
   Route::get('invoice/document/download/all/{link}', 'InvoiceController@downloadZip');
+  Route::get('invoice/download/full/{link}', 'InvoiceController@downloadFullPatron');
   Route::get('invoice/download/{link}', 'InvoiceController@downloadPatron');
   Route::get('invoice/download/{link}/{id}', 'InvoiceController@downloadPatron');
   Route::get('invoice/download/{link}/{id}/{type}', 'InvoiceController@downloadPatron');
-  Route::get('invoice/download/full/{link}', 'InvoiceController@downloadFullPatron');
   Route::get('invoice/document/{link}', 'InvoiceController@documentsPatron');
   Route::get('invoice/document/{link}/{name}', 'InvoiceController@documentsPatron');
   Route::get('invoice/document/{link}/{parent}/{sub}', 'InvoiceController@documentsPatron');
