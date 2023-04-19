624ea868192e39dfa75e5b1e6b85170e5d31ffdc
nitishswipezbriq <nitish.harchand@briq.com>
2023-04-19 10:55:24 +0530

    undo debug

--- resources/views/app/merchant/invoice/Gco-listing/content.blade.php
+++ resources/views/app/merchant/invoice/Gco-listing/content.blade.php
@@ -34,15 +34,14 @@
                         VALUE</td>
                     @foreach ($change_order_columns as $coKeyIndex => $change_order_column)
                     <td class="border-b border-r border-l td-703 font-regular text-xs text-center text-capitalize">
-{{--                        @php--}}
-{{--                            if(is_numeric($coKeyIndex)) {--}}
-{{--                                $coNumber = $coKeyIndex + 1;--}}
-{{--                            } else {--}}
-{{--                                $coNumber = intval($coKeyIndex) + 1;--}}
-{{--                            }--}}
-{{--                        @endphp--}}
-{{--                        {{'CO ' . $coNumber}} --}}
-                        {{ Str::replace('_', ' ', $change_order_column) }}
+                        @php
+                            if(is_numeric($coKeyIndex)) {
+                                $coNumber = $coKeyIndex + 1;
+                            } else {
+                                $coNumber = intval($coKeyIndex) + 1;
+                            }
+                        @endphp
+                        {{'CO ' . $coNumber}}
                     </td>
                     @endforeach    
                     <td class="border-b border-r border-l td-703 font-regular text-xs text-center">
