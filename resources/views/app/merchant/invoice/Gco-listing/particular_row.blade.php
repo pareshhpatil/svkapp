2b474e526daca62cc87c377f12da8f30c201cc29
nitishswipezbriq <nitish.harchand@briq.com>
2023-04-19 10:50:17 +0530

    debug

--- resources/views/app/merchant/invoice/Gco-listing/particular_row.blade.php
+++ resources/views/app/merchant/invoice/Gco-listing/particular_row.blade.php
@@ -2,7 +2,7 @@
     <td class="border-r border-l td-703 text-left">
         <p class="text-sm">{{ $rowArray['code'] }} </p>
     </td>
-    <td class="border-r border-l td-703 text-left second">
+    <td class="border-r border-l td-703 text-left">
         @if (isset($group_name))
             @php $groupName=str_replace(' ', '_', strlen($group_name) > 7 ? substr($group_name, 0, 7) : $group_name) @endphp
             <p class="text-sm">{{ $rowArray['description'] }}  @if (!empty($rowArray['attachment']))
@@ -19,22 +19,22 @@
         @endif
         
     </td>
-    <td class="border-r border-l td-703 text-right">
+    <td class="border-r border-l td-703 text-right original-con">
         <p class="text-sm"><x-amount-format :amount="$rowArray['original_contract_amount']" /></p>
     </td>
     
     @if (isset($rowArray['change_order_col_values']))
         @foreach ($rowArray['change_order_col_values'] as $key => $change_order_col_value)
-            <td class="border-r border-l td-703 text-right">
+            <td class="border-r border-l td-703 text-right co-vals">
                 <p class="text-sm"><x-amount-format :amount="$change_order_col_value" /></p>
             </td>
         @endforeach        
     @endif
 
-    <td class="border-r border-l td-703 text-right">
+    <td class="border-r border-l td-703 text-right approved-co">
         <p class="text-sm"><x-amount-format :amount="$rowArray['approved_change_order_amount']" /></p>
     </td>
-    <td class="border-r border-l td-703 text-right">
+    <td class="border-r border-l td-703 text-right current-amount">
         <p class="text-sm"><x-amount-format :amount="$rowArray['current_contract_amount']" /></p>
     </td>
 
