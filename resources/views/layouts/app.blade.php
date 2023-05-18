3d20299287d543df68586454cc3ef98676b7bec3
Paresh <paresh.patil@briq.com>
2023-05-18 12:06:58 +0530

    hi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,7 @@
         </div>
     </div>
     @else
-    <div class="appHeader no-border transparent position-absolute">
+    <div class="appHeader">
         <div class="left">
             <a href="#" class="headerButton goBack">
                 <ion-icon name="chevron-back-outline"></ion-icon>
