fde7c465700fa255af818556cdf08e31c0572621
Paresh <paresh.patil@briq.com>
2023-05-20 16:46:17 +0530

    fixed

---
+++ resources/views/blog/4.blade.php
@@ -0,0 +1,85 @@
+<div class="section mt-2">
+  <h1>
+    Traffic rules in India
+  </h1>
+  <div class="blog-header-info mt-2 mb-2">
+    <div>
+      <img src="/assets/img/sample/avatar/avatar1.jpg" alt="img" class="imaged w24 rounded me-05">
+      by <a href="#">Paresh Patil</a>
+    </div>
+    <div>
+      18, April 2023
+    </div>
+  </div>
+  <div class="lead">
+    As the bustling streets of India witness an ever-increasing number of vehicles, it becomes crucial for both drivers and pedestrians to abide by traffic rules to ensure safety and maintain smooth traffic flow. In this blog post, we will explore some essential traffic rules in India that every road user should be aware of. Let's dive in!
+
+  </div>
+</div>
+
+<div class="section mt-2">
+  <h3>1. Follow Lane Discipline</h3>
+  <p>
+  Lane discipline is vital for maintaining order on the roads. Stick to your designated lane and avoid unnecessary lane changes. Overtake from the right side of the vehicle ahead only when it is safe to do so.
+  <figure>
+    <img src="/assets/img/blog/rule1.jpg" alt="image" class="imaged img-fluid">
+  </figure>
+  
+  </p>
+
+
+  <h3>2. Use Indicators</h3>
+  <p>
+  Indicators or turn signals are essential for communicating your intentions to other road users. Use them while changing lanes, taking turns, or overtaking. Remember to turn off your indicators after completing the maneuver.
+
+
+  </p>
+  <figure>
+    <img src="/assets/img/blog/rule2.jpg" alt="image" class="imaged img-fluid">
+  </figure>
+  
+  <h3>3. Adhere to Speed Limits</h3>
+  <p>
+  Speed limits are in place to ensure the safety of all road users. Different speed limits are set for different types of roads. Always pay attention to the speed limit signs and adjust your speed accordingly.
+
+  </p>
+  <figure>
+    <img src="/assets/img/blog/rule3.jpg" alt="image" class="imaged img-fluid">
+  </figure>
+
+  <h3>4. Wear Seat Belts</h3>
+  <p>
+  The use of seat belts is mandatory for all occupants of a vehicle. Ensure that everyone is buckled up before starting the journey. Seat belts significantly reduce the risk of injury during accidents.
+  </p>
+  <figure>
+    <img src="/assets/img/blog/rule4.jpg" alt="image" class="imaged img-fluid">
+  </figure>
+  <h3>5. Stop at Red Signals</h3>
+  <p>
+  When approaching a red traffic signal, it is mandatory to stop behind the white stop line or the signal itself. Wait for the green signal before proceeding further, even if there is no traffic coming from other directions.
+  </p>
+  <figure>
+    <img src="/assets/img/blog/rule5.jpg" alt="image" class="imaged img-fluid">
+  </figure>
+  <h3>6. Give Way to Emergency Vehicles</h3>
+  <p>
+  When you hear the siren or see the flashing lights of an emergency vehicle, immediately give way and make space for them to pass. This allows emergency services to reach their destinations promptly.  </p>
+  <figure>
+    <img src="/assets/img/blog/rule6.jpg" alt="image" class="imaged img-fluid">
+  </figure>
+  <h3>7. No Drunk Driving</h3>
+  <p>
+  Driving under the influence of alcohol or drugs is a serious offense. It impairs your judgment and reaction time, increasing the risk of accidents. Never drink and drive, and always designate a sober driver or use alternative transportation.  <figure>
+    <img src="/assets/img/blog/rule7.jpg" alt="image" class="imaged img-fluid">
+  </figure>
+  <h3>8. Follow Pedestrian Crossings</h3>
+  <p>
+  Pedestrians have the right of way at designated crosswalks. Slow down or stop to let them cross safely. Always be cautious, especially near schools, hospitals, and crowded areas.
+  <figure>
+    <img src="/assets/img/blog/rule8.jpg" alt="image" class="imaged img-fluid">
+  </figure>
+  
+
+  <p>Adhering to traffic rules is not only a legal obligation but also a responsibility towards the safety of oneself and others. By following these essential traffic rules in India, we can contribute to creating a safer and more organized road environment. Let's make a conscious effort to be responsible road users and promote a culture of traffic rule compliance.</p>
+
+</div>
