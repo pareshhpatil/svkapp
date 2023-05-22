946fc7a32d417d2dfe4c13ec416cda78e4e9af1b
Paresh <paresh.patil@briq.com>
2023-05-22 23:20:43 +0530

    login error

--- resources/views/passenger/ride-track.blade.php
+++ resources/views/passenger/ride-track.blade.php
@@ -349,7 +349,7 @@
             //getData();
 
             lat=lat-0.00005;
-            map.setCenter(new google.maps.LatLng(lat, lat_long));
+            //map.setCenter(new google.maps.LatLng(lat, lat_long));
             originMarker.setPosition(new google.maps.LatLng(lat, lat_long));
 
             
