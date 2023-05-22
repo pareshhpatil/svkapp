9253701aa50dc2ea5b0b7f392439e962e3254957
Paresh <paresh.patil@briq.com>
2023-05-22 23:25:03 +0530

    login error

--- resources/views/passenger/ride-track.blade.php
+++ resources/views/passenger/ride-track.blade.php
@@ -346,9 +346,9 @@
         });
         setInterval(function() {
 
-            //getData();
+            getData();
 
-            lat=lat-0.00005;
+            //lat=lat-0.00005;
             //map.setCenter(new google.maps.LatLng(lat, lat_long));
             originMarker.setPosition(new google.maps.LatLng(lat, lat_long));
 
