85c29a5a517508fee8bba2081ba2c11f5d7489de
Paresh <paresh.patil@briq.com>
2023-05-22 17:58:58 +0530

    bug fixed

--- resources/views/passenger/ride-track.blade.php
+++ resources/views/passenger/ride-track.blade.php
@@ -44,6 +44,41 @@
         bottom: 30px !important;
         left: 10px !important;
     }
+
+    .wallet-card .wallet-footer .item .icon-wrapper {
+        background: #e8481e;
+        width: 35px;
+        height: 35px;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 10px;
+        color: #fff;
+        font-size: 20px;
+        margin-bottom: 10px;
+    }
+
+    .transactions .item {
+        padding: 10px 24px;
+    }
+
+    .marker-label {
+        margin-top: 80px;
+        font-size: 12px;
+        line-height: 1em;
+        border-radius: 100px;
+        color: #ffffff !important;
+        letter-spacing: 0;
+        height: 22px;
+        min-width: 22px;
+        width: auto;
+        padding: 5px 10px;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        font-weight: 400;
+        background: #1DCC70 !important;
+    }
 </style>
 <script src="https://maps.googleapis.com/maps/api/js?key={{env('MAP_KEY')}}"></script>
 
@@ -55,64 +90,7 @@
 
 
 
-        <div class=" ">
-            <div class="wallet-card" style="box-shadow: none;padding: 0;padding-bottom: 10px;">
-                <!-- Balance -->
-                <!-- Wallet Footer -->
-                <div class="wallet-footer">
-                    <div v-if="data.ride.status==1" class="item mb-1">
-                        <a href="#" data-bs-toggle="modal" data-bs-target="#withdrawActionSheet">
-                            <div class="icon-wrapper bg-success">
-                                <ion-icon name="location-outline"></ion-icon>
-                            </div>
-                            <strong>Track</strong>
-                        </a>
-                    </div>
-                    <div v-if="data.ride.status==1" class="item mb-1">
-                        <a href="#" onclick="start();">
-                            <div class="icon-wrapper bg-success">
-                                <ion-icon name="location-outline"></ion-icon>
-                            </div>
-                            <strong>Track Me</strong>
-                        </a>
-                    </div>
-                    <div class="item">
-                        <a href="#" data-bs-toggle="modal" data-bs-target="#cancelride">
-                            <div class="icon-wrapper bg-primary bg-red" style="background: #e8481e !important;">
-                                <ion-icon name="close-circle-outline"></ion-icon>
-                            </div>
-                            <strong>Cancel</strong>
-                        </a>
-                    </div>
-                    <div class="item">
-                        <a href="#" data-bs-toggle="modal" data-bs-target="#helpmodal">
-                            <div class="icon-wrapper bg-warning">
-                                <ion-icon name="chatbubble-ellipses-outline"></ion-icon>
-                            </div>
-                            <strong>Help</strong>
-                        </a>
-                    </div>
-                    <div class="item">
-                        <a href="#" data-bs-toggle="modal" data-bs-target="#sosmodel">
-                            <div class="icon-wrapper bg-primary bg-red" style="background: #e8481e !important;">
-                                <ion-icon name="accessibility-outline"></ion-icon>
-                            </div>
-                            <strong>SOS</strong>
-                        </a>
-                    </div>
-                    <div class="item">
-                        <a href="whatsapp://send?text=Hey, Please track my ride {{$data['link']}}" data-action="share/whatsapp/share" id="shareBtn">
-                            <div class="icon-wrapper bg-info">
-                                <ion-icon name="logo-whatsapp"></ion-icon>
-                            </div>
-                            <strong>Share</strong>
-                        </a>
-                    </div>
 
-                </div>
-                <!-- * Wallet Footer -->
-            </div>
-        </div>
 
 
 
@@ -122,6 +100,69 @@
     </div>
 
 </div>
+<div class="modal fade action-sheet show" id="actionSheet" tabindex="-1" role="dialog" aria-modal="true" style="display: block;">
+    <div class="modal-dialog" role="document" style="bottom: 50px;">
+        <div class="modal-content">
+
+            <div class="modal-body">
+                <div class="transactions">
+                    <!-- item -->
+                    <a href="app-transaction-detail.html" class="item">
+                        <div class="detail">
+                            <img src="/assets/img/driver.png?v-1" alt="img" class="image-block imaged w48">
+                            <div>
+                                <strong>Nitin Kamble</strong>
+                                <strong class="text-primary">Arriving in 10 min</strong>
+
+                                <p>Shopping</p>
+                            </div>
+                        </div>
+                        <div class="right">
+                            <div class="text-danger"> <ion-icon name="call-outline" style="font-size: 25px;"></ion-icon></div>
+                        </div>
+                    </a>
+                </div>
+                <div class=" ">
+                    <div class="wallet-card" style="box-shadow: none;padding: 0;padding-bottom: 10px;">
+                        <!-- Balance -->
+                        <!-- Wallet Footer -->
+                        <div class="wallet-footer" style="padding-top: 10px;">
+                            <div v-if="data.ride.status==1" class="item mb-1">
+                                <a href="#" data-bs-toggle="modal" data-bs-target="#withdrawActionSheet">
+                                    <div class="icon-wrapper bg-success">
+                                        <ion-icon name="navigate-outline"></ion-icon>
+                                    </div>
+                                    <strong>Navigate</strong>
+                                </a>
+                            </div>
+
+                            <div class="item">
+                                <a href="#" data-bs-toggle="modal" data-bs-target="#helpmodal">
+                                    <div class="icon-wrapper bg-warning">
+                                        <ion-icon name="chatbubble-ellipses-outline"></ion-icon>
+                                    </div>
+                                    <strong>Help</strong>
+                                </a>
+                            </div>
+
+                            <div class="item">
+                                <a href="whatsapp://send?text=Hey, Please track my ride {{$data['link']}}" data-action="share/whatsapp/share" id="shareBtn">
+                                    <div class="icon-wrapper bg-info">
+                                        <ion-icon name="logo-whatsapp"></ion-icon>
+                                    </div>
+                                    <strong>Share</strong>
+                                </a>
+                            </div>
+
+                        </div>
+                        <!-- * Wallet Footer -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
 
 @section('footer')
@@ -202,6 +243,10 @@
                     anchor: new google.maps.Point(0, 32)
                 },
                 position: myLatLng,
+                label: {
+                    text: "Nitin Kamble",
+                    className: 'marker-label'
+                },
                 map: map
             });
 
@@ -234,7 +279,7 @@
             mylocation_lat = posistion.latitude;
             mylocation_long = posistion.longitude;
             var myLatLng = new google.maps.LatLng(mylocation_lat, mylocation_long),
-            myOptions = {
+                myOptions = {
                     zoom: 18,
                     center: myLatLng,
                     mapTypeId: google.maps.MapTypeId.ROADMAP
