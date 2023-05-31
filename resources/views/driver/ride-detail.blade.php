725edcbecb33a71b49197e86970a361b34ab9239
Paresh <paresh.patil@briq.com>
2023-05-31 22:19:29 +0530

    hide export

--- resources/views/driver/ride-detail.blade.php
+++ resources/views/driver/ride-detail.blade.php
@@ -340,10 +340,7 @@
             verifyotp() {
                 var otp = document.getElementById('otp').value;
                 array = this.data.ride_passengers[this.selected_id];
-                if (array.mobile == '') {
-                    array.otp = '';
-                }
-                if (array.otp != otp) {
+                if (array.otp != otp || array.mobile!='') {
                     this.verror = 'Invalid OTP';
                     document.getElementById('otp').value = '';
                 } else {
