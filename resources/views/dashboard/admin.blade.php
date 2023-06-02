e5b4518dd6054c50a6fcd4b0490f30a3f20a9b39
Paresh <pareshhpatil@gmail.com>
2023-06-02 14:28:35 +0530

    hide export

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div class="row">
+@if(Session::get('admin_id')==1)
     <div class="col-lg-3 col-xs-6">
         <!-- small box -->
         <a href="/admin/employee/advance">
@@ -24,6 +25,67 @@
     </div>
     <div class="col-lg-3 col-xs-6">
         <!-- small box -->
+        <a href="/admin/employee/list">
+            <div class="small-box bg-blue">
+                <div class="inner">
+                    <h3>Employee</h3>
+                    <div class="hidden-xs">
+                        <p>&nbsp;</p>
+                        <br>
+                    </div>
+                    <div class="visible-xs">
+                        <p style="font-size: 30px;"><i class="ion ion-ios-speedometer"></i></p>
+                    </div>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-ios-speedometer"></i>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <a href="/admin/employee/absent">
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3>Absent</h3>
+                    <div class="hidden-xs">
+                        <p>&nbsp;</p>
+                        <br>
+                    </div>
+                    <div class="visible-xs">
+                        <p style="font-size: 30px;"><i class="ion ion-android-person"></i></p>
+                    </div>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-android-person"></i>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <a href="/admin/logsheet/approve">
+            <div class="small-box bg-fuchsia">
+                <div class="inner">
+                    <h3>Approve</h3>
+                    <div class="hidden-xs">
+                        <p>&nbsp;</p>
+                        <br>
+                    </div>
+                    <div class="visible-xs">
+                        <p style="font-size: 30px;"><i class="ion ion-ios-checkmark"></i></p>
+                    </div>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-android-checkmark-circle"></i>
+                </div>
+            </div>
+        </a>
+    </div>
+    @endif
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
         <a href="/admin/logsheet">
             <div class="small-box bg-red">
                 <div class="inner">
@@ -43,26 +105,7 @@
         </a>
     </div>
 
-    <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <a href="/admin/employee/list">
-            <div class="small-box bg-blue">
-                <div class="inner">
-                    <h3>Employee</h3>
-                    <div class="hidden-xs">
-                        <p>&nbsp;</p>
-                        <br>
-                    </div>
-                    <div class="visible-xs">
-                        <p style="font-size: 30px;"><i class="ion ion-ios-speedometer"></i></p>
-                    </div>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-ios-speedometer"></i>
-                </div>
-            </div>
-        </a>
-    </div>
+    
 
 
     <div class="col-lg-3 col-xs-6">
@@ -88,26 +131,7 @@
 
 
 
-    <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <a href="/admin/employee/absent">
-            <div class="small-box bg-aqua">
-                <div class="inner">
-                    <h3>Absent</h3>
-                    <div class="hidden-xs">
-                        <p>&nbsp;</p>
-                        <br>
-                    </div>
-                    <div class="visible-xs">
-                        <p style="font-size: 30px;"><i class="ion ion-android-person"></i></p>
-                    </div>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-android-person"></i>
-                </div>
-            </div>
-        </a>
-    </div>
+    
 
 
     <div class="col-lg-3 col-xs-6">
@@ -130,26 +154,7 @@
             </div>
         </a>
     </div>
-    <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <a href="/admin/logsheet/approve">
-            <div class="small-box bg-fuchsia">
-                <div class="inner">
-                    <h3>Approve</h3>
-                    <div class="hidden-xs">
-                        <p>&nbsp;</p>
-                        <br>
-                    </div>
-                    <div class="visible-xs">
-                        <p style="font-size: 30px;"><i class="ion ion-ios-checkmark"></i></p>
-                    </div>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-android-checkmark-circle"></i>
-                </div>
-            </div>
-        </a>
-    </div>
+    
 
 
 </div>
