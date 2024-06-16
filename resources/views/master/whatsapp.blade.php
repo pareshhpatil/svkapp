ec60d6fdcd20bfed7ea0583a2aea2bec4d6a7ac4
Paresh <paresh.patil@briq.com>
2024-06-16 14:27:07 +0530

    co and cost transactions

--- resources/views/master/whatsapp.blade.php
+++ resources/views/master/whatsapp.blade.php
@@ -51,7 +51,7 @@
 
                     </div>
                     <div v-if="item.message_type=='video'">
-                        <video  controls>
+                        <video  width="200"  controls>
                             <source :src="item.message" type="video/mp4">
                         </video>
                         <a :href="item.message">Video</a>
