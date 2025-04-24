4611b8ae5ea1cf001d9bc699f5948fba417310fb
Paresh Patil <pares@MacBook-Pro.local>
2025-04-24 10:49:16 +0530

    pdf margin dynamic

--- app/Http/Lib/Encryption.php
+++ app/Http/Lib/Encryption.php
@@ -1,15 +1,18 @@
 <?php
 namespace App\Http\Lib;
- class Encryption {
 
-    public static function safe_b64encode($string) {
+class Encryption
+{
+    public static function safe_b64encode($string)
+    {
         $data = base64_encode($string);
-        $data = str_replace(array('+', '/', '='), array('-', '_', ''), $data);
+        $data = str_replace(['+', '/', '='], ['-', '_', ''], $data);
         return $data;
     }
 
-    public static function safe_b64decode($string) {
-        $data = str_replace(array('-', '_'), array('+', '/'), $string);
+    public static function safe_b64decode($string)
+    {
+        $data = str_replace(['-', '_'], ['+', '/'], $string);
         $mod4 = strlen($data) % 4;
         if ($mod4) {
             $data .= substr('====', $mod4);
@@ -17,22 +20,45 @@ namespace App\Http\Lib;
         return base64_decode($data);
     }
 
-    public static function encode($value) {
+    protected static function getKey()
+    {
+        $key = base64_decode(env('ENCKEY'));
+        if (strlen($key) !== SODIUM_CRYPTO_SECRETBOX_KEYBYTES) {
+            throw new \Exception('Invalid encryption key length');
+        }
+        return $key;
+    }
+
+    protected static function getNonce()
+    {
+        $nonce = base64_decode(env('NOUNCE'));
+        if (strlen($nonce) !== SODIUM_CRYPTO_SECRETBOX_NONCEBYTES) {
+            throw new \Exception('Invalid nonce length');
+        }
+        return $nonce;
+    }
+
+    public static function encode($value)
+    {
         if (!$value) {
             return false;
         }
-        $value=(string)$value;
-        $ciphertext = sodium_crypto_secretbox($value, env('NOUNCE'), env('ENCKEY'));
+        $value = (string) $value;
+        $ciphertext = sodium_crypto_secretbox($value, self::getNonce(), self::getKey());
         return trim(self::safe_b64encode($ciphertext));
     }
 
-    public static function decode($value) {
+    public static function decode($value)
+    {
         if (!$value) {
             return false;
         }
         $ciphertext = self::safe_b64decode($value);
-        $decrypttext = sodium_crypto_secretbox_open($ciphertext, env('NOUNCE'), env('ENCKEY'));
-        return trim($decrypttext);
+        $decrypted = sodium_crypto_secretbox_open($ciphertext, self::getNonce(), self::getKey());
+        if ($decrypted === false) {
+            throw new \Exception('Decryption failed');
+        }
+        return trim($decrypted);
     }
-
 }
+
