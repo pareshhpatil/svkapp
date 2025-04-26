d0b05f989773cfc8553737f2234ace5a4031e803
Paresh Patil <pares@MacBook-Pro.local>
2025-04-26 16:16:36 +0530

    round issue fixed

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -142,11 +142,11 @@
                         <ul class="navbar-nav flex-row align-items-center ms-auto">
 
                             <!-- Quick links  -->
-                            <li class="nav-item dropdown-shortcuts navbar-dropdown dropdown me-2 me-xl-0">
+                            <!-- <li class="nav-item dropdown-shortcuts navbar-dropdown dropdown me-2 me-xl-0">
                                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
                                     <i class="ti ti-layout-grid-add ti-md"></i>
                                 </a>
-                                <!-- <div class="dropdown-menu dropdown-menu-end py-0">
+                                <div class="dropdown-menu dropdown-menu-end py-0">
                                     <div class="dropdown-menu-header border-bottom">
                                         <div class="dropdown-header d-flex align-items-center py-3">
                                             <h5 class="text-body mb-0 me-auto">Shortcuts</h5>
@@ -172,8 +172,8 @@
                                         </div>
 
                                     </div>
-                                </div> -->
-                            </li>
+                                </div>
+                            </li> -->
                             <!-- Quick links -->
 
                             <!-- Notification -->
