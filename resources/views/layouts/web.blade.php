0c87c4aaf52a5138900bdf18f40cca769b188d16
Paresh Patil <pares@MacBook-Pro.local>
2025-04-26 16:15:58 +0530

    round issue fixed

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -146,14 +146,14 @@
                                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
                                     <i class="ti ti-layout-grid-add ti-md"></i>
                                 </a>
-                                <div class="dropdown-menu dropdown-menu-end py-0">
+                                <!-- <div class="dropdown-menu dropdown-menu-end py-0">
                                     <div class="dropdown-menu-header border-bottom">
                                         <div class="dropdown-header d-flex align-items-center py-3">
                                             <h5 class="text-body mb-0 me-auto">Shortcuts</h5>
                                             <a href="javascript:void(0)" class="dropdown-shortcuts-add text-body" data-bs-toggle="tooltip" data-bs-placement="top" title="Add shortcuts"><i class="ti ti-sm ti-apps"></i></a>
                                         </div>
                                     </div>
-                                    <!-- <div class="dropdown-shortcuts-list scrollable-container">
+                                    <div class="dropdown-shortcuts-list scrollable-container">
                                         <div class="row row-bordered overflow-visible g-0">
                                             <div class="dropdown-shortcuts-item col">
                                                 <span class="dropdown-shortcuts-icon rounded-circle mb-2">
@@ -171,8 +171,8 @@
                                             </div>
                                         </div>
 
-                                    </div> -->
-                                </div>
+                                    </div>
+                                </div> -->
                             </li>
                             <!-- Quick links -->
 
