6e8af99a6058933373aca7a277b268498efcba8d
Paresh Patil <pares@MacBook-Pro.local>
2025-05-22 18:15:12 +0530

    docusign changes

--- app/Models/RideModel.php
+++ app/Models/RideModel.php
@@ -17,6 +17,7 @@ class RideModel extends ParentModel
                     ->where("r.user_type",  5);
             })
             ->where('p.is_active', 1)
+            ->where('p.passenger_type', 1)
             ->where('p.ride_id', $ride_id)
             ->select(DB::raw('p.id,pr.address,pr.mobile ,p.status,p.otp,TIME_FORMAT(p.pickup_time, "%h %i %p") as pickup_time ,rating,TIME_FORMAT(p.cab_time, "%h %i %p") as cab_time ,TIME_FORMAT(p.in_time, "%h %i %p") as in_time ,TIME_FORMAT(p.drop_time, "%h %i %p") as drop_time ,
             p.pickup_location,p.drop_location,r.icon,pr.location,pr.employee_name as name,pr.gender,p.passenger_id,p.actual_pickup_location,p.actual_drop_location'))
